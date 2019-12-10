a79b74d70821760401f8897d707808ca8b3a885f
刘旭东 <liuxudong@trendlab.ai>
2019-12-10 14:27:34 +0800

    - add 跳转时传值

--- lib/demos/list_demo.dart
+++ lib/demos/list_demo.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -12,29 +13,11 @@ class RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   _pushSaved(BuildContext context) async {
+    // P 跳转到下一个页面，并等待回传
     final result = await Navigator.of(context).push(
       new MaterialPageRoute(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
-            return new ListTile(
-              title: new Text(pair.asPascalCase, style: _biggerFont),
-            );
-          });
-          final List<Widget> divided =
-              ListTile.divideTiles(tiles: tiles, context: context).toList();
-          return WillPopScope(
-            onWillPop: () async {
-              Navigator.of(context).pop('一共收藏了${divided.length}条信息');
-              return false;
-            },
-            child: new Scaffold(
-              appBar: new AppBar(
-                title: const Text('Saved Suggestions'),
-              ),
-              body: new ListView(children: divided),
-            ),
-          );
-        },
+        // P 跳转到下一个页面，并传值
+        builder: (BuildContext context) => SavedRandomWords(saved: _saved,), 
       ),
     );
     if (result != null) {
@@ -147,6 +130,56 @@ class RandomWordsState extends State<RandomWords> {
       body: Builder(
         builder: (context) => _buildSuggestions(context),
       ),
+    );
+  }
+}
+
+class SavedRandomWords extends StatelessWidget {
+  final Set<WordPair> saved;
+  SavedRandomWords({Key key, @required this.saved}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Iterable<ListTile> tiles = saved.map((WordPair pair) {
+      return new ListTile(
+        title: new Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => SuggestionDetail(),
+              // P 跳转时，填入需要传入的值
+              // settings: RouteSettings(arguments: pair),
+            )
+          );
+        },
+      );
+    });
+    final List<Widget> divided =
+        ListTile.divideTiles(tiles: tiles, context: context).toList();
+    // P 定义WillPopScope，重写back方法，并传值
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.of(context).pop('一共收藏了${divided.length}条信息');
+        return false;
+      },
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Saved Suggestions'),
+        ),
+        body: new ListView(children: divided),
+      ),
+    );
+  }
+}
+
+class SuggestionDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // P 使用setting方法接受值, 注意：不是每次跳到此页面时都会有值，所以在使用时要判断
+    final WordPair wordPair = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: AppBar(title: Text('${wordPair != null ? wordPair.asPascalCase : ''}')),
+      body: Center(child: Text('Suggestion Detail', style: TextStyle(fontSize: 32),),),
     );
   }
 }
